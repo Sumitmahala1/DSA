c9a9010a67dacea2d5e22c2ede979a5e7ecbaa7d
Sumit Mahala <sumitforwork11@gmail.com>
2026-08-25 13:19:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (52.98%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -3,9 +3,6 @@ class Solution:
         if n <= 0:
             return False
 
-        while n % 2 == 0:
-            n = n // 2
-
-        return n == 1    
+        return (n & (n - 1)) == 0   
 
        
